f7f8225b5baca63f51da2c60702a3de2cfda1deb
gsapia <guillermosapia.lsp@hotmail.com.ar>
2020-11-09 00:03:45 -0300

    subo todo el proyecto terminado

---
+++ src/main/java/com/market/domain/service/UserDetailService.java
@@ -0,0 +1,18 @@
+package com.market.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("user", "{noop}123456", new ArrayList<>());
+    }
+}
